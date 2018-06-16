61991024b5f356471a107dcc8bd53635cc04354c
mkmehta5470 <40265936+mkmehta5470@users.noreply.github.com>
2018-06-16 18:36:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Amarnath Rana</title>
+<meta name="keywords" content="Best engineer, software engineer,Quassarian-Innovations Pvt. Ltd., Webgax.com, Best A.I Developer, Best Python Developer, Best website developer, Motivator">
+<meta name="description" content="amarnath.gq is a website which is for self portfolio,  where describe about myself, that what I do, and where from I compeleted my education, What skill I have,and what I do for any organisation, Through this website I represent my-self with public.">
+<meta name="author" content="Amarnath Rana">
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css"  href="css1/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css"  href="css1/style.css">
+<link rel="stylesheet" type="text/css" href="css1/prettyPhoto.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:400,700,900,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="js1/modernizr.custom.js"></script>
+
+
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <!--[endif]-->
+</head>
+<body>
+<!-- Navigation
+    ==========================================-->
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+  <div class="container"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" href="index.php">Amarnath Rana</a> </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="home.php" class="page-scroll">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  
+  <!-- /.container-fluid --> 
+</nav>
+
+
+<!-- Header -->
+<header class="text-center" name="home">
+  <div class="intro-text">
+    <marquee behavior="alternate"><h1>We are <span class="color">Quassarian</span></h1></marquee>
+    <p style="font-size:20px; color:#ffffff"> Our Company famous for digital design and development of <br>
+	 Dyanamic Websites, E-commerce Websites, Mobile Applications, Android App, PC Software, and many other types of IT services.</p>
+	 
+	   <div class="clearfix"></div>
+    <a href="registration.html" class="btn btn-default btn-lg">Call Back request</a> </div>
+</header>
+<!-- About Section -->
+<div id="about-section">
+  <div class="container">
+    <div class="section-title">
+      <h2>About Us</h2>
+      <hr>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-md-4">
+        <h4>Who We Are</h4>
+        <p>We are <strong> <i>Quassarians</i></strong>, Our Company famous for digital design and development of <br>
+	 Dyanamic Websites, E-commerce Websites, Mobile Applications, Android App, A.I Applications, Mobile Games, PC Software, IoT Gadgets and many other types of IT services.<br>
+         </div>
+      <div class="col-md-4">
+        <h4>What We Do</h4>
+        <p>Quassarians  famous for new digital design and development of <br>
+	 Dyanamic Websites, E-commerce Websites, Mobile Applications, Android App, PC Software, and many other types of IT services. Quassarians also famous for Innovations of many IoT devices, Drones,  </p>
+      </div>
+      <div class="col-md-4">
+        <h4>Why Choose Us</h4>
+        <p>Quassarians thats means a person who have a unique thoughts, always believes on give best services to clients, for long term relationship.  </p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Services Section -->
+<div id="services-section">
+  <div class="container">
+    <div class="section-title">
+      <h2>Our Services</h2>
+      <hr>
+    </div>
+    <div class="space"></div>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-desktop"></i>
+        <h4>Web design</h4>
+        <p>We are design and development of <br>
+	  Static Websites, Semi-Dyanamic Websites, Dyanamic Websites,<br>
+	  semi-eCommerce Websites, E-commerce Websites, with SEO. </p>
+      </div>
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-gears"></i>
+        <h4>App Development</h4>
+        <p>We are designed and development of many types of Android App for many organisations.</p>
+      </div>
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-pie-chart"></i>
+        <h4>Analystics</h4>
+        <p>Quassarians going to starts a new IT services that is Data-Analystics. <B><i>Comming Soon* </i></B></p>
+      </div>
+	  </div>
+	  <div class="space"></div>
+    <div class="row">
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-line-chart"></i>
+        <h4>Digital-Marketing</h4>
+        <p>We also provide digital Marketing to promote  that website online. </p>
+      </div>
+    
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-shopping-cart"></i>
+        <h4>eCommerce</h4>
+        <p>We are design and development of <br>
+	   Dyanamic Websites,
+	  semi-eCommerce Websites, E-commerce Websites, with SEO. <br>
+      	In which we give a server, unlimited Web-Emails, Unlimited Data-storages, 18 months free services.   </p>
+      </div>
+      <div class="col-md-4 col-sm-6 service"> <i class="fa fa-file-text-o"></i>
+        <h4>Content Development</h4>
+        <p>We are also develop content for Organisations.</p>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- Portfolio Section -->
+<div id="works-section" style="margin-bottom:10px; margin-top:10px;">
+  <div class="container">
+  <!-- Container -->
+    <div class="section-title">
+      <h2>Our Portfolio</h2>
+      <hr>
+      <div class="clearfix"></div>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">All</a></li>
+            <li><a href="#" data-filter=".web">Web Design</a></li>
+            <li><a href="#" data-filter=".app">App Development</a></li>
+            <li><a href="#" data-filter=".branding">Branding</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/1.png" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Successzone</h4>
+                <small>www.successzone.org.in is a education poratl dynamic websites which is for educations purpose, </small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/1.png" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/02.png" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Qgax</h4>
+                <small>www.qgax.webgax.com that is Employee management Applications Developed By Webgax-Quassarians Innovations Pvt. Ltd. </small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/02.png" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/03.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Portfolio Design </h4>
+                <small>Quassarians also design Portfolio for organisationsnisation.</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/03.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/4.png" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>AishaGlobal</h4>
+                <small>www.aishaglobal.in this website developed for Aisha Global Financial services.</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/4.png" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/05.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Bootstrap Websites</h4>
+                <small>Our Websites is full dyanmic and responsive websites.</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/05.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/6.png" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Website form</h4>
+                <small> Amarnath Rana</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/6.png" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 branding app">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/07.jpg" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>Bootstrap websites</h4>
+                <small></small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/07.jpg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img1/portfolio/8.png" rel="prettyPhoto">
+              <div class="hover-text">
+                <h4>About Us page </h4>
+                <small>www.successzone.org.in about us page.</small>
+                <div class="clearfix"></div>
+              </div>
+              <img src="img1/portfolio/8.png" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Contact Section -->
+<div id="contact-section">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Contact Us</h2>
+      <hr>
+    </div>
+    <div class="col-md-4">
+      <h4>Contact info</h4>
+      <div class="space"></div>
+      <p><i class="fa fa-map-marker"></i>Pathaldiha, Koderma <br>
+        &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; 825410, 
+		Jharkhand</p>
+      <div class="space"></div>
+      <p><i class="fa fa-envelope-o"></i>amar74.soft@gmail.com</p>
+      <div class="space"></div>
+      <p><i class="fa fa-phone"></i>+91-7404664714, &nbsp; &nbsp;
+	  +91-8295307908</p>
+    </div>
+    <div class="col-md-8">
+      <h4>Leave us a message</h4>
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Name" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="E-mail....." required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-default">Send Message</button>
+      </form>
+	 </div>
+  </div>
+</div>
+	<div id="social-section">
+  <div class="container">
+    <div class="social">
+      <ul>
+        <li  style="background-color:#b0ce84"><a href="https://www.facebook.com/amarnath77492""><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+        <li  style="background-color:#b0ce84"><a href="https://twitter.com/Amarnathhawkin2"><i class="fa fa-twitter"></i></a></li>
+        <li  style="background-color:#b0ce84"><a href="https://plus.google.com/104329251458258968638"><i class="fa fa-dribbble"></i></a></li>
+        <li  style="background-color:#b0ce84"><a href="https://github.com/amar74"><i class="fa fa-github"></i></a></li>
+        <li  style="background-color:#b0ce84"><a href="https://www.instagram.com/eramarnath/?hl=en"><i class="fa fa-instagram"></i></a></li>
+        <li  style="background-color:#b0ce84"><a href="https://in.linkedin.com/in/amarnath-rana-639736117"><i class="fa fa-linkedin"></i></a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<div id="footer">
+  <div class="container">
+    <p>Copyright &copy; 2018 Amarnath Rana. Designed by <a href="http://www.amarnath.com" rel="nofollow">Amarnath Rana</a></p>
+  </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> 
+<script type="text/javascript" src="js1/jquery.1.11.1.js"></script>
+<script type="text/javascript" src="js1/bootstrap.js"></script> 
+<script type="text/javascript" src="js1/SmoothScroll.js"></script> 
+<script type="text/javascript" src="js1/jquery.prettyPhoto.js"></script> 
+<script type="text/javascript" src="js1/jquery.isotope.js"></script> 
+<script type="text/javascript" src="js1/jqBootstrapValidation.js"></script> 
+<script type="text/javascript" src="js1/contact_me.js"></script> 
+
+<!-- Javascripts --> 
+<script type="text/javascript" src="js1/main.js"></script>
+</body>
+</html>
